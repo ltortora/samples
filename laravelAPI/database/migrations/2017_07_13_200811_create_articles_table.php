d164011756060cbfc050535999f24536807fc731
ltortora <lucastortora@gmail.com>
2017-07-13 17:12:05 -0300

    migrations update

--- laravelAPI/database/migrations/2017_07_13_200811_create_articles_table.php
+++ laravelAPI/database/migrations/2017_07_13_200811_create_articles_table.php
@@ -13,7 +13,16 @@ class CreateArticlesTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('articles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('excerpts');
+            $table->text('body');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -23,6 +32,6 @@ class CreateArticlesTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::drop('articles');
     }
 }
